a74971040cf8b44c3ca6466eee9c17e72f15a1d2
NickFornit <fornit@scorcher.ru>
2022-08-23 17:47:26 +0300

    Типа редактирую функцию
    Что быдет если ты тоже ее будешь редактировать?

--- index.php
+++ index.php
@@ -7,4 +7,11 @@ header("HTTP/1.1 301 Moved Permanently");
 header ( 'Location: /pult.php' );
 exit("4444444444 555555555555 55555");
 
+//типа редактирую функцию
+function aaaa($n)
+{
+$n++;
+return $n;
+}
+
 

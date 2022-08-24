696f8f87f911d628fee7a94bc1d472460042d9a5
NickFornit <fornit@scorcher.ru>
2022-08-24 15:07:20 +0300

    новая ветка

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 */
 header("HTTP/1.1 301 Moved Permanently");
 header ( 'Location: /pult.php' );
-exit("4444444444 555555555555 55555 666666 77777");
+exit("4444444444 555555555555 55555 666666 77777 9999");
 
 //типа редактирую функцию
 function aaaa($n)

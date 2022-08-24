3d31ebf258e40fb978bb66746c2cedb1472c1f79
NickFornit <fornit@scorcher.ru>
2022-08-24 15:23:48 +0300

    пояснения тестируемой версии

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
 /*       http://go/  работа с данными для  Beast
 
-2222222222222 33333333333333333333
+Это - тестируемая версия
 */
 header("HTTP/1.1 301 Moved Permanently");
 header ( 'Location: /pult.php' );
